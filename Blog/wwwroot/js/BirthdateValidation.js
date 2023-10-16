12a3bbc62c2ec8bbb6f3cc7ee84c9fa7a4708874
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-10-16 10:52:46 +0500

    datepicker value fix

--- Blog/wwwroot/js/BirthdateValidation.js
+++ Blog/wwwroot/js/BirthdateValidation.js
@@ -1,14 +1,18 @@
-﻿    function GetBirthdayDateLimits() {
-        let dateForm = document.getElementById("birthdate");
-        const date = new Date();
-        let currentDay = String(date.getDate()).padStart(2, '0');
+﻿function GetBirthdayDateLimits() {
+    let dateForm = document.getElementById("birthdate");
+    const date = new Date();
+    let currentDay = String(date.getDate()).padStart(2, '0');
 
-        let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+    let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
 
-        let currentYear = date.getFullYear();
+    let currentYear = date.getFullYear();
 
-        // min age for registration 14 years old
-        dateForm.min = `${currentYear - 100}-${currentMonth}-${currentDay}`;
-        dateForm.max = `${currentYear - 14}-${currentMonth}-${currentDay}`;
-    }
-    onload = GetBirthdayDateLimits();
+    // min age for registration 14 years old
+    dateForm.min = `${currentYear - 100}-${currentMonth}-${currentDay}`;
+    dateForm.max = `${currentYear - 14}-${currentMonth}-${currentDay}`;
+    dateForm.setAttribute('value', dateForm.max)
+    dateForm.datepicker({
+        showOn: "focus" // Открывать datepicker при фокусировке на поле ввода
+    })
+}
+onload = GetBirthdayDateLimits();
